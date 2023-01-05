a640aea2c18a8f7e5c2b5c1d3a89e8f574362f40
baksohyeon <bori12370@gmail.com>
2023-01-05 17:52:44 +0900

    // WIP: 간단 api 쏴주는 로그인 구현

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,27 +1,28 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import { isDataView } from "util/types";
 import Layout from "../components/layout";
 import { getUserProfile } from "../lib/auth";
 
-export async function getServerSideProps(context) {
-  // Call the 'getUserProfile' function to retrieve the user's profile data
-
-  const userProfile = await getUserProfile();
-  return {
-    props: {
-      userProfile,
+export async function getServerSideProps({ req }) {
+  const res = await axios.get("http://localhost:3001/api/auth/google/profile", {
+    withCredentials: true,
+    headers: {
+      Cookie: req.headers.cookie,
     },
-  };
+  });
+  const data = await res.data;
+  return { props: { data } };
 }
 
-export default function ProfilePage({ userProfile }: { userProfile: any }) {
+export default function ProfilePage({ data }: { data: any }) {
   // Render the user's profile data on the page
-  console.log(userProfile);
   return (
     <Layout>
       <h1>Profile</h1>
-      {/* <p>Name: {userProfile.username}</p>
-      <p>Email: {userProfile.email}</p> */}
+      <p>Name: {data.username}</p>
+      <p>Email: {data.email}</p>
+      <p>date of joining : {data.createdAt}</p>
     </Layout>
   );
 }
