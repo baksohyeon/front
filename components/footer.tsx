7f323d4be17883966d9ba45533122fbeeb1d0e02
baksohyeon <bori12370@gmail.com>
2023-03-03 13:50:18 +0900

    refactor: 필요없는 페이지 삭제

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,15 @@ export default function Footer() {
       <hr />
       <ul className={styles.navItems}>
         <li className={styles.navItem}>
-          <a href="https://dorito-dev.tistory.com/">Tistory</a>
+          <a href="https://dorito-dev.tistory.com">Tistory</a>
         </li>
         <li className={styles.navItem}>|</li>
         <li className={styles.navItem}>
-          <a href="https://github.com/baksohyeon">Dorito's GitHub</a>
+          <a href="https://github.com/baksohyeon">Dorito GitHub</a>
+        </li>
+        <li className={styles.navItem}>|</li>
+        <li className={styles.navItem}>
+          <a href="https://dorito.netlify.app/">Dorito PKM</a>
         </li>
       </ul>
     </footer>
